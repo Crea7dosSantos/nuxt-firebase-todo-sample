29eb70a32471370d19e6740a84e9b946d92def0c
Crea7dosSantos <cr26yusaku0720@icloud.com>
2019-08-02 03:13:20 +0900

    create todos.js

---
+++ store/todos.js
@@ -0,0 +1,35 @@
+import firebase from '~/plugins/firebase'
+import { firestoreAction } from 'vuexfire'
+
+const db = firebase.firestore()
+const todosRef = db.collection('todos')
+
+// todo一覧を管理するtodosを配列で定義する
+export default state = () => ({
+    todos: []
+})
+
+export const actions = {
+    // 初期化
+    init: firestoreAction(({ bindFirestoreRef }) => {
+        bindFirestoreRef('todos', todosRef)   // bindFirestoreRefにbindしたいdbのnameと参照先を渡すとstateにデータがbindされる
+    }),
+    // 追加
+    add: firestoreAction((context, name) => {
+        if (name.trim()) {
+            todosRef.add({
+                name: name,
+                done: false,
+                created: firebase.firestore.FieldValue.serverTimestamp()
+            })
+        }
+    }),
+    remove: firestoreAction((context, id) => {
+        todosRef.doc(id).delete()
+    }),
+    toggle: firestoreAction((context, todo) => {
+        todosRef.doc(todo.id).update({
+            done: !todo.done
+        })
+    })
+}
